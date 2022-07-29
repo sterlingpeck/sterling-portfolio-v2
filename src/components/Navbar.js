4e94b4fb8d7dec2f362c7fdef2d171810cbdf46f
Sterling Peck <peck.sterling@gmail.com>
2022-07-29 09:57:23 -0600

    navbar design and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <header>
       <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
@@ -13,13 +16,31 @@ const Navbar = () => {
             <ul className="hidden md:flex">
               <li>About</li>
               <li>Projects</li>
-              <li>Github</li>
               <li>Resume</li>
-              <li>Contact Me</li>
             </ul>
           </div>
+          <div className="hidden md:flex pr-4">
+            <button className="border-none bg-transparent text-black mr-4">
+              Github
+            </button>
+            <button className="px-8 py-3">Contact Me</button>
+          </div>
+          <div className="md:hidden" onClick={handleClick}>
+            {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
+          </div>
         </div>
-        <MenuIcon className="w-5" />
+
+        <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+          <li className="border-b-2 border-zinc-300 w-full">About</li>
+          <li className="border-b-2 border-zinc-300 w-full">Projects</li>
+          <li className="border-b-2 border-zinc-300 w-full">Resume</li>
+          <div className="flex flex-col">
+            <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
+              Github
+            </button>
+            <button className="px-8 py-3">Contact Me</button>
+          </div>
+        </ul>
       </div>
       {/* 
       <nav className="">
